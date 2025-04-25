d890162ff243dbbfc0f4ecc54b1d46af6ba2bf09
itzsusko <luansusko@gmail.com>
2025-04-24 22:44:03 -0300

    update 22:43

--- src/routes/estoque/cardEstoque.routes.js
+++ src/routes/estoque/cardEstoque.routes.js
@@ -1,24 +1,28 @@
+
+// src/routes/estoque/cardEstoque.routes.js
 import express from 'express'
 import {
   listarCards,
   criarCard,
   deletarCard,
   editarCard,
+  listarItensDoCard,
+  criarItemNoCard,
   editarItem,
+  deletarItem,
 } from '../../controllers/estoque/CardEstoque.controller.js'
 import upload from '../../middleware/upload.js'
-import { listarItensDoCard } from '../../controllers/estoque/CardEstoque.controller.js'
-import { criarItemNoCard } from '../../controllers/estoque/CardEstoque.controller.js'
 
 const router = express.Router()
 
-// Definir as rotas
 router.get('/', listarCards)
 router.post('/', upload.single('imagem'), criarCard)
 router.delete('/:id', deletarCard)
 router.put('/:id', upload.single('imagem'), editarCard)
+
 router.get('/:id/itens', listarItensDoCard)
 router.post('/:id/itens', criarItemNoCard)
-router.put('/cards/:cardId/itens/:itemId', editarItem) // Corrigindo para a rota de edição de item
+router.put('/cards/:cardId/itens/:itemId', editarItem)
+router.delete('/cards/:cardId/itens/:itemId', deletarItem)
 
 export default router
